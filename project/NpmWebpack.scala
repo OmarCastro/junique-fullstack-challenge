63a74a6902d3dee1e58b3cb02679d01d1ed66e09
OmarCastro <omar.castro.360@gmail.com>
2020-01-20 13:31:44 +0000

    fix: Exec "npm ci" on sbt run
    When cloning the project and run "sbt run", the WebpackHook runs npm
    script "build:dev" without node_modules folder created, failing webpack
    execution. The solution is to run "npm ci" each time "sbt run" is executed.
    
    - Configure WebpackHook to run "npm ci" before start
    
    - Code review

--- project/NpmWebpack.scala
+++ project/NpmWebpack.scala
@@ -11,26 +11,28 @@ object NpmWebpack {
       var process: Option[Process] = None
 
       val shell: Seq[String] = if (sys.props("os.name").contains("Windows")) Seq("cmd", "/c") else Seq("bash", "-c")
-      val npmInstall: Seq[String] = shell :+ "npm install"
-      val npmTest: Seq[String] = shell :+    "npm run test"
-      val npmLint: Seq[String] = shell :+    "npm run lint"
+      val npmCi: Seq[String] = shell :+ "npm ci"
       val npmBuildDev: Seq[String] = shell :+   "npm run build:dev"
       val npmRunDev: Seq[String] = shell :+   "npm run watch:dev"
 
 
-      override def beforeStarted() = {
+      override def beforeStarted(): Unit = {
+        val npmCiExitCode = Process(npmCi, base).run().exitValue
+        if(npmCiExitCode != 0){
+          throw new RuntimeException("Error running npm ci")
+        }
         process = Option(
           Process(npmBuildDev, base).run()
         )
       }
 
-      override def afterStarted() = {
+      override def afterStarted(): Unit = {
         process = Option(
           Process(npmRunDev, base).run()
         )
       }
 
-      override def afterStopped() = {
+      override def afterStopped(): Unit = {
         process.foreach(_.destroy())
         process = None
       }
